9471604754f2d2b39eb56e95529cdb9aefe93ddf
Akhila <palleakhila007@gmail.com>
2024-11-19 23:14:11 +0530

    First commit

---
+++ samplemavenJava/src/test/main/java/com/app/demo/samplemavenJava/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.samplemavenJava;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
